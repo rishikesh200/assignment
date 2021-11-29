0e9e93923b58120f665a914e0aebefb3ecbb4632
rishikesh200 <93971335+rishikesh200@users.noreply.github.com>
2021-11-29 10:35:24 +0530

    Add files via upload

---
+++ module4/script.js
@@ -0,0 +1,19 @@
+var names=new Array();
+names[0]="yaakov";
+names[1]="james";
+names[2]="jos";
+names[3]="ram";
+names[4]="john";
+names[5]="sam";
+names[6]="jim";
+names[7]="jack";
+names[8]="smith";
+names[9]="jason";
+for (let i = 0;i < names.length;i++) {
+    if (names[i].charAt(0)==='j' || names[i].charAt(0)==='J') {
+        console.log("goodbye "+names[i])
+    }
+    else{
+        console.log("hello "+names[i])
+  }
+}
